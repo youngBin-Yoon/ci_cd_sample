ce21e8bc249b9cf3570e746bcfc010f0c6119bd1
윤영빈 <ybyoon@m-ultiply.com>
2024-02-29 14:10:59 +0900

    fix swagger

--- src/main/java/com/example/ci_cd_sample/config/SwaggerConfig.java
+++ src/main/java/com/example/ci_cd_sample/config/SwaggerConfig.java
@@ -1,6 +1,11 @@
+package com.example.ci_cd_sample.config;
+
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -8,15 +13,14 @@ import org.springframework.context.annotation.Configuration;
 public class SwaggerConfig {
     @Bean
     public OpenAPI openAPI() {
-        return new OpenAPI()
-                .components(new Components())
-                .info(apiInfo());
-    }
 
-    private Info apiInfo() {
-        return new Info()
+        Info info = new Info()
                 .title("Springdoc 테스트")
                 .description("Springdoc을 사용한 Swagger UI 테스트")
-                .version("1.0.0");
+                .version("v1.0.0");
+        return new OpenAPI()
+                .components(new Components())
+                .info(info);
     }
 }
+
--- src/main/java/com/example/ci_cd_sample/controller/ItemController.java
+++ src/main/java/com/example/ci_cd_sample/controller/ItemController.java
@@ -16,34 +16,32 @@ import java.util.Optional;
 @RequestMapping("/items")
 public class ItemController {
     private final ItemService itemService;
-
+    @Operation(summary = "대시보드 - 총DB수 조회" , description = "대시보드 - 총DB수 조회",
+            parameters = {
+                    @Parameter(name = "startDate", description = "시작일", example = "2023-05-01"),
+                    @Parameter(name = "endDate", description = "종료일", example = "2023-05-31")
+            })
+    @GetMapping(value = "/get-total-db-count")
+    public List<Item>  getAllItems() {
+        return itemService.findAll();
+    }
     public ItemController(ItemService itemService) {
         this.itemService = itemService;
     }
-    @Operation(summary = "문자열 반복", description = "파라미터로 받은 문자열을 2번 반복합니다.")
-    @Parameter(name = "str", description = "2번 반복할 문자열")
-    @GetMapping
-    public List<Item> getAllItems() {
-        return itemService.findAll();
-    }
 
-    @Operation(summary = "문자열 반복", description = "파라미터로 받은 문자열을 2번 반복합니다.")
-    @Parameter(name = "str", description = "2번 반복할 문자열")
     @GetMapping("/{id}")
     public ResponseEntity<Item> getItemById(@PathVariable Long id) {
         Optional<Item> item = itemService.findById(id);
         return item.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 
-    @Operation(summary = "문자열 반복", description = "파라미터로 받은 문자열을 2번 반복합니다.")
-    @Parameter(name = "str", description = "2번 반복할 문자열")
+
     @PostMapping
     public Item createItem(@RequestBody Item item) {
         return itemService.save(item);
     }
 
-    @Operation(summary = "문자열 반복", description = "파라미터로 받은 문자열을 2번 반복합니다.")
-    @Parameter(name = "str", description = "2번 반복할 문자열")
+
     @PutMapping("/{id}")
     public ResponseEntity<Item> updateItem(@PathVariable Long id, @RequestBody Item item) {
         return itemService.findById(id)
@@ -54,8 +52,7 @@ public class ItemController {
                 }).orElseGet(() -> ResponseEntity.notFound().build());
     }
 
-    @Operation(summary = "문자열 반복", description = "파라미터로 받은 문자열을 2번 반복합니다.")
-    @Parameter(name = "str", description = "2번 반복할 문자열")
+
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteItem(@PathVariable Long id) {
         return itemService.findById(id)
